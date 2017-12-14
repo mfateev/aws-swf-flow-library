ff5c3116c8c3f7327875dd09c29eeb8bc3aa8819
Maxim Fateev <maxim@uber.com>
2017-12-13 21:07:17 -0800

    Added WorkflowFuture

--- src/main/java/com/uber/cadence/internal/dispatcher/Workflow.java
+++ src/main/java/com/uber/cadence/internal/dispatcher/Workflow.java
@@ -1,5 +1,6 @@
 package com.uber.cadence.internal.dispatcher;
 
+import java.util.concurrent.Future;
 import java.util.concurrent.locks.Lock;
 
 public class Workflow {
@@ -16,6 +17,13 @@ public class Workflow {
     }
 
 
+    /**
+     * Note that workflow executes all threads one at a time, ensures that they are interrupted
+     * only when blocked on something like Lock or {@link Future#get()} and uses memory barrier to ensure
+     * that all variables are accessible from any thread. So Lock is needed only in rare cases when critical
+     * section invokes blocking operations.
+     * @return Lock implementation that can be used inside a workflow code.
+     */
     public static Lock newReentrantLock() {
         return new LockImpl();
     }
